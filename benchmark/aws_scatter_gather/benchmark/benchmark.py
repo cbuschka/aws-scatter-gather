c096b86a87b1a3107640677835dfb48c3c23b69f
conni <cbuschka@gmail.com>
2020-06-20 12:43:50 +0200

    Added rounds to benchmark.

--- benchmark/aws_scatter_gather/benchmark/benchmark.py
+++ benchmark/aws_scatter_gather/benchmark/benchmark.py
@@ -3,20 +3,24 @@ from uuid import uuid4
 
 from aws_scatter_gather.benchmark import s3_sqs_lambda_async
 from aws_scatter_gather.benchmark import s3_sqs_lambda_sync
+from aws_scatter_gather.util.trace import trace
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-RECORD_COUNT = 10001
-
 
 def run():
-    batch = {"records": [{"index": i, "info": "request#{}".format(i)} for i in range(RECORD_COUNT)]}
-    batch_id = str(uuid4())
-    s3_sqs_lambda_sync.run(batch_id, batch)
+    for count in [11, 101, 1001]:
+        logger.info("Benchmarking with {count} records.".format(count=count))
+        batch = {"records": [{"index": i, "info": "request#{}".format(i)} for i in range(count)]}
 
-    batch_id = str(uuid4())
-    s3_sqs_lambda_async.run(batch_id, batch)
+        with trace("Variant s3_sqs_lambda_sync..."):
+            batch_id = str(uuid4())
+            s3_sqs_lambda_sync.run(batch_id, batch)
+
+        with trace("Variant s3_sqs_lambda_async..."):
+            batch_id = str(uuid4())
+            s3_sqs_lambda_async.run(batch_id, batch)
 
 
 if __name__ == "__main__":
