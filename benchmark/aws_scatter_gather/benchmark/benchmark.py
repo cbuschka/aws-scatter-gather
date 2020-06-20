67838f08086eda74791537df7e8eb4558e8a6344
conni <cbuschka@gmail.com>
2020-06-20 12:07:48 +0200

    Increased benchmark count to 10001.

--- benchmark/aws_scatter_gather/benchmark/benchmark.py
+++ benchmark/aws_scatter_gather/benchmark/benchmark.py
@@ -2,18 +2,18 @@ import logging
 from uuid import uuid4
 
 from aws_scatter_gather.benchmark import s3_sqs_lambda_async
-#from aws_scatter_gather.benchmark import s3_sqs_lambda_sync
+from aws_scatter_gather.benchmark import s3_sqs_lambda_sync
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-RECORD_COUNT = 1001
+RECORD_COUNT = 10001
 
 
 def run():
     batch = {"records": [{"index": i, "info": "request#{}".format(i)} for i in range(RECORD_COUNT)]}
     batch_id = str(uuid4())
-    #s3_sqs_lambda_sync.run(batch_id, batch)
+    s3_sqs_lambda_sync.run(batch_id, batch)
 
     batch_id = str(uuid4())
     s3_sqs_lambda_async.run(batch_id, batch)
